6fe853567c7640506b7f614838344548ff99ac31
chae03yb <chae03yb@gmail.com>
2020-08-20 15:41:27 +0900

    First Commit

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import sys
 import sqlite3
+from datetime import datetime
 
 parser = argparse.ArgumentParser(description="시간표 도움말",)
 
@@ -10,17 +11,35 @@ parser.add_argument("-e", "--edit", dest="edit", action="store_true", required=F
 parser.add_argument("-d", "--dele", dest="dele", action="store_true", required=False, help="시간표를 삭제합니다.")
 
 args = parser.parse_args()
+con = sqlite3.connect("Timetable.sqlite3")
+cur = con.cursor()
+
+Day = ["Mon", "Tue", "Wed", "Thu", "Fri"]
 
 if args.view:
-    con = sqlite3.connect("Timetable.sqlite3")
-    cur = con.cursor()
+    if datetime.today().weekday() > 4:
+        print("주말입니다.")
+    else:
+        with con:
+            print("\n".join(map(str, cur.execute(f"select * from {Day[datetime.today().weekday()]};")))\
+                .replace("('", '').replace("',)", ''))
 
-    with con:
-        pass
-        # cur.execute()
 if args.edit:
-    print("edit")
+    with con:
+        for i in range(0, 5):
+            for x in range(1, 8):
+                Data = input(f"{Day[i]} - {x}교시의 과목을 입력해주세요.")
+                cur.execute(f"insert into {Day[i]} values (?);", (Data,))
+        con.commit()
+
 if args.dele:
-    print("dele")
-# class ArgAction(argparse.Action):
-    # def __init__(self)
+    check = input("정말로 시간표를 삭제하시겠습니까?\nY/N: ")
+    if check == "Y":
+        with con:
+            for i in range(0, 5):
+                cur.execute(f"drop table {Day[i]};")
+            print("시간표를 삭제했습니다.")
+            con.commit()
+        os.remove("Timetable.sqlite3")
+    else:
+        print("취소되었습니다.")
--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 import os
 import sqlite3
+from datetime import datetime
 
 con = sqlite3.connect("Timetable.sqlite3")
 cur = con.cursor()
 
+Day = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+
 with con:
-    cur.execute("create table Mon (str Subjects);")
-    cur.execute("create table Tue (str Subjects);")
-    cur.execute("create table Wed (str Subjects);")
-    cur.execute("create table Thu (str Subjects);")
-    cur.execute("create table Fri (str Subjects);")
-    con.commit()
+    try:
+        for i in range(0, 5):
+            cur.execute(f"create table {Day[i]} (str Subjects);")
+        print("설정이 완료되었습니다.")
+    except IndexError:
+        pass
+    finally:
+        con.commit()
